276084b4e07e11e50ba97bfa59de344a90c51a50
Andrei-Daniel Anton <andr3y_121@yahoo.com>
2014-07-14 09:05:00 +0300

    initial import.

---
+++ Matchismo/Model/PlayingCardDeck.m
@@ -0,0 +1,34 @@
+//
+//  PlayingCardDeck.m
+//  Matchismo
+//
+//  Created by Andrei-Daniel Anton on 07/07/14.
+//  Copyright (c) 2014 Andrei-Daniel Anton. All rights reserved.
+//
+
+#import "PlayingCardDeck.h"
+#import "PlayingCards.h"
+
+@implementation PlayingCardDeck
+
+-(instancetype)init{
+    
+    self=[super init];
+    
+    if(self){
+        
+        for(NSString *suit in [PlayingCards validSuits])
+        {
+            for(NSUInteger rank=1; rank<=[PlayingCards maxRank]; rank++)
+            {
+                PlayingCards *card=[[PlayingCards alloc]init];
+                card.rank=rank;
+                card.suit=suit;
+                [self addCard:card];
+            }
+        }
+    }
+    return self;
+}
+
+@end
